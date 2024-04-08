66fec139b9a4d16fb403b658d72ae5c541f71599
Leonardo Baigorria <leo_baigorria99@hotmail.com>
2024-04-07 22:38:07 -0300

    Guardado de ejercicios 07/04

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,19 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+    // Verificamos si el tipo de dato de "numero" es un número.
+    if (typeof numero !== "number") {
+      return false;
+    }
+  
+    // Verificamos si el resto de la división de "numero" por 1 es igual a 0.
+    if (numero % 1 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+
+  
 }
 
 module.exports = esNumeroEntero;
